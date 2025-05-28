8fa48064977744d017cefb7b04823687c1fc335c
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-05-28 15:28:10 -0500

    Controles para video

--- ProyectoIntegracion/ViewControllerVideo.swift
+++ ProyectoIntegracion/ViewControllerVideo.swift
@@ -1,29 +1,36 @@
-//
-//  ViewControllerVideo.swift
-//  ProyectoIntegracion
-//
-//  Created by Dnilson Achahuanco on 28/05/25.
-//
-
 import UIKit
+import AVKit
 
 class ViewControllerVideo: UIViewController {
+    var urlVideo: URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        reproducirVideo()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    func reproducirVideo() {
+        guard let url = urlVideo else { return }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let player = AVPlayer(url: url)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+
+        let botonCerrar = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(cerrarReproductor))
+        playerController.navigationItem.rightBarButtonItem = botonCerrar
+
+        let navController = UINavigationController(rootViewController: playerController)
+
+        navController.navigationBar.isHidden = false
+
+        DispatchQueue.main.async {
+            self.present(navController, animated: true) {
+                player.play()
+            }
+        }
     }
-    */
 
+    @objc func cerrarReproductor() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- ProyectoIntegracion/VistaReproductorVideo.swift
+++ ProyectoIntegracion/VistaReproductorVideo.swift
@@ -5,4 +5,19 @@
 //  Created by Dnilson Achahuanco on 28/05/25.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+import AVKit
+
+struct VistaReproductorVideo: UIViewControllerRepresentable {
+    var url: URL
+
+    func makeUIViewController(context: Context) -> ViewControllerVideo {
+        let controlador = ViewControllerVideo()
+        controlador.urlVideo = url
+        return controlador
+    }
+
+    func updateUIViewController(_ uiViewController: ViewControllerVideo, context: Context) {
+    }
+}
