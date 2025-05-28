4d890a2ec120dea80539fa3a2bc35732a2a80c09
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-05-28 13:55:56 -0500

    Initial Commit

---
+++ ProyectoIntegracion/ProyectoIntegracionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoIntegracionApp.swift
+//  ProyectoIntegracion
+//
+//  Created by Dnilson Achahuanco on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoIntegracionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
